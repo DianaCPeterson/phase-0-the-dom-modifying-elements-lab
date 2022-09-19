7ec211e96780ec63b493b903ee35b5363519f9c7
Diana C Peterson <dpeter10@gmail.com>
2022-09-19 17:00:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your code here!
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('H1')
+
+newHeader.id = 'victory'
+newHeader.innerHTML = "Diana is the champion."
+//       it("has a 'newHeader' variable that points to an <h1> node", () => {(
+          
+//       (newHeader.nodeName).eql("H1");
+//       }
+      
+//       );
+  
+//       it("the 'newHeader' variable that points to the <h1> node has an id of 'victory'", () => {
+//         expect(
+//           newHeader.id,
+//           "Make sure the id of your <h1> is set to 'victory'"
+//         ).eql("victory");
+//       });
+  
+//       it("the 'newHeader' variable that points to the <h1> node with an id of 'victory' has the text \"YOUR-NAME is the champion\" (with your name in place of YOUR-NAME) inside it", () => {
+//         expect(newHeader.innerHTML, "Make sure your <h1> with id 'victory' includes the text \"is the champion\" inside it").to.include("is the champion");
+//       });
+  
+//     });
+//   })
